d5acf5c7ed0fb3355c2104f2f9b059bd62b7f0d4
lizandraligia <lizandraligia@hotmail.com>
2018-01-21 20:48:27 -0200

    correções

--- src/gui/Main.java
+++ src/gui/Main.java
@@ -1,18 +1,27 @@
 package gui;
 	
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
 
 
 public class Main extends Application {
+	
+private static BorderPane root = new BorderPane();
+	
 	@Override
 	public void start(Stage primaryStage) {
 		try {
-			BorderPane root = new BorderPane();
-			Scene scene = new Scene(root,400,400);
+			this.root = FXMLLoader.load(getClass().getResource("/gui/TelaInicio.fxml"));
+			Scene scene = new Scene(root,1020,680);
 			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+			primaryStage.setTitle("Donaso");
+			primaryStage.getIcons().add(new Image("/imagens/logo.png"));
+			primaryStage.setResizable(false);
+			//primaryStage.setMaximized(true);
 			primaryStage.setScene(scene);
 			primaryStage.show();
 		} catch(Exception e) {
@@ -23,4 +32,9 @@ public class Main extends Application {
 	public static void main(String[] args) {
 		launch(args);
 	}
+	
+	public static BorderPane getRoot() { 
+		return root; 
+	} 
+	
 }
